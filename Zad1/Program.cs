b5ee3db21ff2c9737107257f1620c7ced98e1832
Valentin Loboda <vloboda@etfos.hr>
2020-04-16 11:58:48 +0200

    Add project files.

---
+++ Zad1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+           
+
+            DiceRoller dices = new DiceRoller();
+
+            int numberOfDices = 20;
+
+            for (int i = 0; i < numberOfDices; i++) {
+
+                dices.InsertDie(new Die(6));
+            
+            }
+
+            dices.RollAllDice();
+
+            IList<int> rolledValues = dices.GetRollingResults();
+
+            for (int i = 0; i<rolledValues.Count; i++) {
+
+                Console.WriteLine(rolledValues[i]);
+            
+            }
+        }
+    }
+}
